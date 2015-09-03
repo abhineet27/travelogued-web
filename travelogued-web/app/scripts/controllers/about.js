76491d35185c93fd25e9aba96f91eddd5fb15d65
abhineet <abhineet.kumar@snapdeal.com>
2015-09-03 15:00:53 +0530

    first commit

---
+++ travelogued-web/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name traveloguedWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the traveloguedWebApp
+ */
+angular.module('traveloguedWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ travelogued-web/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name traveloguedWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the traveloguedWebApp
+ */
+angular.module('traveloguedWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
